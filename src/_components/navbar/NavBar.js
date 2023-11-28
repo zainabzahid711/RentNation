30b2b03233e3bd3a08169103cd41d75cb856b79f
zainabzahid711 <zainabzahid711@gmail.com>
2023-11-28 10:16:05 +0500

    navBar btn added via props

--- src/_components/navbar/NavBar.js
+++ src/_components/navbar/NavBar.js
@@ -1,25 +1,22 @@
-import React from "react"
-import Logo from '../../_assets/svg/logo.svg'
-import styles from './NavBar.module.css'
-import Button from "../button/Button"
+import React from "react";
+import Logo from "../../_assets/svg/logo.svg";
+import styles from "./NavBar.module.css";
+import Button from "../button/Button";
 
 const NavBar = () => {
-
-  return(
+  return (
     <div className={styles.navBar}>
-
-    <img className={styles.logo} src={Logo} alt="company-logo" />
+      <img className={styles.logo} src={Logo} alt="company-logo" />
 
       <div className={styles.menu}>
         <a className={styles.menuItem}> HOME </a>
         <a className={styles.menuItem}>LISTINGS</a>
         <a className={styles.menuItem}>ABOUT</a>
         <a className={styles.menuItem}>CONTACT</a>
-        <Button/>
+        <Button title="LOGIN / SIGN UP" />
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default NavBar
+export default NavBar;
