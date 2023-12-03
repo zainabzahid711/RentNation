6009dd092a851bb6a72d53dcf46438c39f98fbd6
zainabzahid711 <zainabzahid711@gmail.com>
2023-12-03 23:38:24 +0500

    responsive styling done

--- src/_components/footer/Footer.js
+++ src/_components/footer/Footer.js
@@ -16,65 +16,64 @@ const Footer = () => {
           </p>
         </div>
         <div className={styles.footerItems}>
-          <ul>
-            <li>
-              <a className={styles.listHeading}> QUICK LINKS </a>
-            </li>
-            <li>
-              <a> Cars for rent </a>
-            </li>
-            <li>
-              <a> Beach for rent </a>
-            </li>
-            <li>
-              <a> Kayaks for rent </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a className={styles.listHeading}> MENEHARIYA </a>
-            </li>
-            <li>
-              <a> Contact Us </a>
-            </li>
-            <li>
-              <a>About Us </a>
-            </li>
-            <li>
-              <a> Terms & Condition </a>
-            </li>
-            <li>
-              <a> Privacy Policy </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a className={styles.listHeading}> TRENDING SEARCH </a>
-            </li>
-            <li>
-              <a> Lorem Ipsum </a>
-            </li>
-            <li>
-              <a> Lorem Ipsum </a>
-            </li>
-            <li>
-              <a> Lorem Ipsum </a>
-            </li>
-            <li>
-              <a> Lorem Ipsum </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a className={styles.listHeading}> SOCIAL MEDIA</a>
-            </li>
+          <div className={styles.footerList}>
+            <h4 className={styles.listHeading}>Quick Links</h4>
+            <ul>
+              <li>
+                <a className={styles.listItem}> Cars for rent </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Beach for rent </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Kayaks for rent </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footerListM}>
+            <h4 className={styles.listHeading}>Menehariya</h4>
+            <ul className={styles.methernal}>
+              <li>
+                <a className={styles.listItem}> Contact Us </a>
+              </li>
+              <li>
+                <a className={styles.listItem}>About Us </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Terms & Condition </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Privacy Policy </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footerListT}>
+            <h4 className={styles.listHeading}>Trending search</h4>
+            <ul>
+              <li>
+                <a className={styles.listItem}> Lorem Ipsum </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Lorem Ipsum </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Lorem Ipsum </a>
+              </li>
+              <li>
+                <a className={styles.listItem}> Lorem Ipsum </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footerList}>
+            <h4 className={styles.listHeading}>Social Medias</h4>
             <img
               className={styles.socialLogos}
               src={socialLogos}
               alt="social-logo"
             />
-          </ul>
+          </div>
         </div>
+        {/* this */}
       </div>
       <p className={styles.lastText}>© Rentnation2023</p>
     </div>
