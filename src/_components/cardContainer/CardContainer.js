5b0722ddfe45948c5a8d36f39a09adec70bf5868
zainabzahid711 <zainabzahid711@gmail.com>
2023-12-07 01:45:41 +0500

    mapping cards

--- src/_components/cardContainer/CardContainer.js
+++ src/_components/cardContainer/CardContainer.js
@@ -4,23 +4,74 @@ import Card from "../card/Card";
 import poloCars from "../../_assets/png/polo cars.png";
 import blurCars from "../../_assets/png/blur cars.png";
 
+const cardData = [
+  {
+    image: poloCars,
+    price: "40",
+    location: "germany",
+    heading: "Golf Items",
+  },
+  {
+    image: blurCars,
+    price: "50",
+    location: "france",
+    heading: "Golf Card",
+  },
+  {
+    image: poloCars,
+    price: "40",
+    location: "germany",
+    heading: "Golf Items",
+  },
+  {
+    image: blurCars,
+    price: "50",
+    location: "france",
+    heading: "Golf Card",
+  },
+  {
+    image: poloCars,
+    price: "40",
+    location: "germany",
+    heading: "Golf Items",
+  },
+  {
+    image: blurCars,
+    price: "50",
+    location: "france",
+    heading: "Golf Card",
+  },
+  {
+    image: poloCars,
+    price: "40",
+    location: "germany",
+    heading: "Golf Items",
+  },
+  {
+    image: blurCars,
+    price: "50",
+    location: "france",
+    heading: "Golf Card",
+  },
+];
+
 const CardContainer = () => {
   return (
     <div className={styles.cardContainer}>
       <h1 className={styles.mainHeading}> Featured Golf Cart for Rent </h1>
       <div className={styles.cardWrapper}>
-        <Card
-          img={poloCars}
-          price={"40"}
-          location={"germany"}
-          heading={"Golf Items"}
-        />
-        <Card
-          img={blurCars}
-          price={"30"}
-          location={"france"}
-          heading={"Golf Card"}
-        />
+        {/* card mapping start */}
+
+        {cardData.map((item) => (
+          <Card
+            img={item.image}
+            price={item.price}
+            location={item.location}
+            heading={item.heading}
+          />
+        ))}
+
+        {/* end */}
       </div>
       <p className={styles.seeMore}> See More </p>
     </div>
--- src/page/FeaturePage.js
+++ src/page/FeaturePage.js
@@ -2,13 +2,16 @@ import React from "react";
 import NavBar from "../_components/navbar/NavBar";
 import CardContainer from "../_components/cardContainer/CardContainer";
 import Footer from "../_components/footer/Footer";
+import BgImage from "../_assets/png/bg-img.png";
+import styles from "./FeaturePage.module.css";
 
 const FeaturePage = () => {
   return (
-    <div>
+    <div className={styles.parent}>
       <NavBar />
       <CardContainer />
       <Footer />
+      <img className={styles.bgImage} src={BgImage} alt="bgImg" />
     </div>
   );
 };
