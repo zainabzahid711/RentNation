fa9078282defbf81c1a1fc934d9e2d1a6926ac7b
zainabzahid711 <zainabzahid711@gmail.com>
2023-12-08 23:16:31 +0500

    array mapping

--- src/_components/orderCardContainer/OrderCardContainer.js
+++ src/_components/orderCardContainer/OrderCardContainer.js
@@ -2,14 +2,37 @@ import React from "react";
 import styles from "./OrderCardContainer.module.css";
 import OrderCard from "../card/OrderCard";
 import mainbikes from "../../_assets/png/bikes.png";
+import BoatsPicture from "../../_assets/png/boatsImage.png";
+import MasksPicture from "../../_assets/png/masksImage.png";
 
 // //array passing
 
-// const OrdercardArry = [
-//   {
-//     image: mainbikes,
-//   }
-// ]
+const OrdercardArry = [
+  {
+    image: mainbikes,
+    heading: "Midsize suv",
+    people: "5 people",
+    limits: "Unlimited Milage",
+    location: "Alibey island",
+    views: "569",
+  },
+  {
+    image: BoatsPicture,
+    heading: "Kayaks",
+    people: "2 people",
+    limits: "limit exceeded",
+    location: "Alibey island",
+    views: "569",
+  },
+  {
+    image: MasksPicture,
+    heading: "beache Supplies",
+    people: "5 people",
+    limits: "limited",
+    location: "Alibey island",
+    views: "569",
+  },
+];
 const OrderCardContainer = () => {
   return (
     <div className={styles.cardContainer}>
@@ -19,15 +42,30 @@ const OrderCardContainer = () => {
         <span className={styles.clordSpan}> See All </span>
       </div>
       <div className={styles.cardWrapper}>
-        <OrderCard
-          img={mainbikes}
-          heading="Midsize suv"
-          people="5 people"
-          limits="Unlimited Milage"
-          location="Alibey island"
-          views="569"
-        />
+        {/* cards mapping */}
+        {OrdercardArry.map((item) => (
+          <OrderCard
+            img={item.image}
+            heading={item.heading}
+            people={item.people}
+            limits={item.limits}
+            location={item.location}
+            views={item.views}
+          />
+        ))}
+        {/* mapping end */}
       </div>
+      <div className={styles.cardBooking}>
+        <span> Waiver </span>
+      </div>
+      <OrderCard
+        img={mainbikes}
+        heading="Damage Report"
+        people="5 people"
+        limits="limit exceeded"
+        location="Pakistan"
+        views="only 432"
+      />
     </div>
   );
 };
--- src/page/OrderPage/OrderPage.js
+++ src/page/OrderPage/OrderPage.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import styles from "./OrderPage.module.css";
 import NavBar from "../../_components/navbar/NavBar";
+import BgImage from "../../_assets/png/bg-img.png";
+import RightHalf from "../../_assets/png/rightHalfcircle.png";
 import OrderCardContainer from "../../_components/orderCardContainer/OrderCardContainer";
 
 const OrderPage = () => {
@@ -9,6 +11,12 @@ const OrderPage = () => {
     <div>
       <NavBar />
       <OrderCardContainer />
+      <img className={styles.BgLeft} src={BgImage} alt="leftHalfcircle"></img>
+      <img
+        className={styles.Bgright}
+        src={RightHalf}
+        alt="rightHalfcircle"
+      ></img>
     </div>
   );
 };
