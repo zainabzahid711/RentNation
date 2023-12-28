e99de6f78fb7845bb79839ddd6c6f799bf04ea57
zainabzahid711 <zainabzahid711@gmail.com>
2023-12-29 03:31:43 +0500

    newComponent ContactUs

---
+++ src/_components/ContactSection/ContactUs.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./ContactUs.module.css";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h1>Get Cars, Beaches and Kayaks on Rent</h1>
+      <p>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Proin sodales ultrices nulla blandit volutpat Vestib ulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae.
+      </p>
+    </div>
+  );
+};
+
+export default ContactUs;
