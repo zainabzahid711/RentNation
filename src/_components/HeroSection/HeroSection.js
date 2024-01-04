c646d4703d42995dc120967e547dc2fcdc89ee59
zainabzahid711 <zainabzahid711@gmail.com>
2024-01-04 22:51:03 +0500

    InputField component imported

--- src/_components/HeroSection/HeroSection.js
+++ src/_components/HeroSection/HeroSection.js
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./HeroSection.module.css";
 import BackgroundImg from "../../_assets/png/bgImage.png";
 import Button from "../button/Button";
-import locationIcon from "../../_assets/svg/location-black.svg";
 import searchIcon from "../../_assets/svg/search-Icon.svg";
+import Input from "../inputType/Input";
 // import { Dropdown } from "react-bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -58,48 +58,13 @@ const HeroSection = () => {
         </h1>
         <p className={styles.heroPara}>
           Welcome to RentNation, where your next beach adventure begins! Whether
-          you're craving a relaxing day by the shore <br></br> or an exciting
-          water exploration, we've got you covered. With our convenient rental
-          services, you can choose between <br></br> multiple fantastic options
+          you're craving a relaxing day by the shore or an exciting water
+          exploration, we've got you covered. With our convenient rental
+          services, you can choose between multiple fantastic options
         </p>
       </div>
       <div className={styles.inputContainer}>
-        <div className={styles.locationDrop}>
-          <img
-            src={locationIcon}
-            alt="Location Icon"
-            className={styles.locationIcon}
-          />
-          <select
-            className={styles.locationSelect}
-            id="location"
-            name="location"
-          >
-            <option value="location">Pakistan</option>
-            <option value="location">Germany</option>
-            <option value="location">Hawaii</option>
-            <option value="location"> Islands</option>
-            <option value="location"> Alaska</option>
-            <option value="location">Canada</option>
-            <option value="location"> Arizona</option>
-            <option value="location">USA</option>
-
-            <option value="location"> Caracas</option>
-            <option value="location"> Mumbai</option>
-            <option value="location"> foundland</option>
-            <option value="location">Georgea</option>
-            <option value="location"> Atlantic</option>
-            <option value="location">Azores</option>
-            <option value="location" selected>
-              Location
-            </option>
-          </select>
-          {/* <LocationDropdown /> */}
-        </div>
-        <input
-          className={styles.heroInput}
-          placeholder="Find beach, kayaks and cars"
-        ></input>
+        <Input />
         <Button
           title="Search"
           background="#FFAE1B"
